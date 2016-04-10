52cd256c320c30f54d1ae35da8d0b3422ee34e5d
Alice Suckling <alicesuckling@live.co.uk>
2016-04-10 15:27:11 +0100

    fixing and learning layout

--- app/scripts/constants/TIMER.js
+++ app/scripts/constants/TIMER.js
@@ -2,11 +2,11 @@
   function TIMER() {
     var TIMER = {};
     
-    TIMER.SESSION = 5,
-    TIMER.BREAK = 2,
+    TIMER.SESSION = 1500,
+    TIMER.BREAK = 300,
     TIMER.SESSION_MESSAGE = "Click to start Work",
     TIMER.BREAK_MESSAGE ="Click to start Break",
-    TIMER.LONGER_BREAK = 10,
+    TIMER.LONGER_BREAK = 1800,
     TIMER.SESSION_COUNT = 4,
     TIMER.LONGER_BREAK_MESSAGE = "Click to start a longer break"
 
